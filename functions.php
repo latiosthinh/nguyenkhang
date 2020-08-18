dcc389ccbdcbbb40152fab6c4477165b3f6b3a52
Thomas <latiosthinh@live.com>
2020-08-18 11:55:47 +0700

    fix slider style

--- functions.php
+++ functions.php
@@ -6,13 +6,12 @@
 **/
 add_action( 'woocommerce_after_shop_loop_item_title', 'shop_sku' );
 function shop_sku(){
-global $product;
-echo '<div class="sku">Mã: ' . get_post_meta( get_the_ID(), '_sku', true ) . '</br>
-		<span>*Liên hệ Hotline để có giá tốt nhất</span>
-	 </div>
-	 <div class="btn-more">
-		<a class="btn-hotline">Hotline</a>
-		<a class="btn-readmore" href="' . get_the_permalink() . '">Chi tiết</a>
-	</div>';
-
+	global $product;
+	echo '<div class="sku">Mã: ' . get_post_meta( get_the_ID(), '_sku', true ) . '</br>
+			<span>*Liên hệ Hotline để có giá tốt nhất</span>
+		</div>
+		<div class="btn-more">
+			<a class="btn-hotline">Hotline</a>
+			<a class="btn-readmore" href="' . get_the_permalink() . '">Chi tiết</a>
+		</div>';
 }
